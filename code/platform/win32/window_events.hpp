a5a49ad1a51da653c9721da463091e4e508dbceb
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-12 22:03:54 +0300

    Add window resizing support and event processing
    Introduced `window_size` struct to manage window dimensions, replacing hardcoded values. Modified event processing to handle `WM_CLOSE` and `WM_SIZE` messages. Updated `Window` class to include size management.

--- code/platform/win32/window_events.hpp
+++ code/platform/win32/window_events.hpp
@@ -7,8 +7,8 @@ namespace engine::win32
     class WindowEvents final : public core::WindowEvents
     {
     public:
-        void update() const override;
+        void update()  const override;
 
-        static LRESULT process(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+        static LRESULT process(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
     };
 }
