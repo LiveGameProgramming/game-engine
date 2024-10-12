7a0333095c891205ea8b0e9e18dfdd11f763ebb1
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-12 22:47:35 +0300

    Add window factory implementation for extensible window creation
    This commit introduces a new window factory pattern to handle window creation, supporting multiple platforms. It includes the necessary classes and enums, updates to CMakeLists, and integration with the existing window management system.

---
+++ code/platform/win32/factory.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "core/factory.hpp"
+
+namespace engine::win32
+{
+    class Factory final : public core::Factory
+    {
+    public:
+        [[nodiscard]] std::unique_ptr<core::Window>       create_window() override;
+        [[nodiscard]] std::unique_ptr<core::WindowEvents> create_events() override;
+    };
+}
