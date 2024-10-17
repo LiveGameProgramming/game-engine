9cd8813b94da8c1eaf91b81e1771a2ef02dddc50
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-17 13:06:48 +0300

    Rename 'load_extended' to 'load_functions'
    Updated the `FunctionsLoader` class to rename the `load_extended` method to `load_functions` for better clarity and consistency. This change is applied across context.cpp, functions_loader.cpp, and functions_loader.hpp files.

--- code/platform/win32/functions_loader.cpp
+++ code/platform/win32/functions_loader.cpp
@@ -3,7 +3,7 @@
 
 namespace engine::win32
 {
-    void FunctionsLoader::load_extended()
+    void FunctionsLoader::load_functions()
     {
         wglCreateContextAttribs = reinterpret_cast<PFNWGLCREATECONTEXTATTRIBSARBPROC>(wglGetProcAddress("wglCreateContextAttribsARB"));
         wglChoosePixelFormat    = reinterpret_cast<PFNWGLCHOOSEPIXELFORMATARBPROC>(wglGetProcAddress("wglChoosePixelFormatARB"));
