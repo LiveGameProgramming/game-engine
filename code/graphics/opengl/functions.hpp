74741a7006dc412d3d68567d3538a5a075b0f6a4
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-17 22:27:38 +0300

    added srgb framebuffers

--- code/graphics/opengl/functions.hpp
+++ code/graphics/opengl/functions.hpp
@@ -4,7 +4,9 @@ namespace engine::gl
 {
     using  PFNGLCLEARPROC      = void(APIENTRY*)(uint32_t flags);
     using  PFNGLCLEARCOLORPROC = void(APIENTRY*)(float r, float g, float b, float a);
+    using  PFNGLENABLEPROC     = void(APIENTRY*)(uint32_t flag);
 
     inline PFNGLCLEARPROC        glClear;
     inline PFNGLCLEARCOLORPROC   glClearColor;
+    inline PFNGLENABLEPROC       glEnable;
 }
