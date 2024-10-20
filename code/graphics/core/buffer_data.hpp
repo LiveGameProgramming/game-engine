465ebfe53b0c4004dd6f2aa83660775f220fcc6d
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-20 19:43:26 +0300

    Refactor template parameters to use lowercase
    Renamed template parameter types in Singleton and BufferData classes to adhere to lowercase naming conventions. This improves code consistency and readability across the project.

--- code/graphics/core/buffer_data.hpp
+++ code/graphics/core/buffer_data.hpp
@@ -8,9 +8,9 @@ namespace engine::core
         [[nodiscard]] const void* data() const;
         [[nodiscard]] uint32_t    size() const;
 
-        template <typename Type>  static BufferData create(const std::vector<Type>& buffer)
+        template <typename type>  static BufferData create(const std::vector<type>& buffer)
         {
-            return { buffer.data(), static_cast<uint32_t>(buffer.size() * sizeof(Type)) };
+            return { buffer.data(), static_cast<uint32_t>(buffer.size() * sizeof(type)) };
         }
 
     private:
