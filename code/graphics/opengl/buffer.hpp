54a49323123bf68b94d91f85acbde9eca96f802a
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-20 19:43:45 +0300

    Align method signatures in Buffer class
    Corrected spacing inconsistencies in the `create` and `destroy` method signatures to align with the code style guidelines. This change improves code readability and maintains uniform formatting across the file.

--- code/graphics/opengl/buffer.hpp
+++ code/graphics/opengl/buffer.hpp
@@ -10,8 +10,8 @@ namespace engine::gl
     class Buffer final : public Object
     {
     public:
-        void create()  override;
-        void destroy() override;
+        void create()   override;
+        void destroy()  override;
 
         void data(const core::BufferData& buffer, uint32_t usage = static_draw) const;
     };
