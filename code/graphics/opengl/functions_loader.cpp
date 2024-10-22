e7e0e112ce97c6b2454cb64054a8c78a1894ca44
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-22 10:28:25 +0300

    Add glDisable function and multisample macro
    Implemented glDisable function in functions_loader and functions.hpp to manage OpenGL states. Introduced the multisample constant in macros.hpp for enhanced rendering options.

--- code/graphics/opengl/functions_loader.cpp
+++ code/graphics/opengl/functions_loader.cpp
@@ -9,6 +9,7 @@ namespace engine::gl
         glClear      = reinterpret_cast<PFNGLCLEARPROC>(GetProcAddress(instance,      "glClear"));
         glClearColor = reinterpret_cast<PFNGLCLEARCOLORPROC>(GetProcAddress(instance, "glClearColor"));
         glEnable     = reinterpret_cast<PFNGLENABLEPROC>(GetProcAddress(instance,     "glEnable"));
+        glDisable    = reinterpret_cast<PFNGLDISABLEPROC>(GetProcAddress(instance,    "glDisable"));
         glDrawArrays = reinterpret_cast<PFNGLDRAWARRAYSPROC>(GetProcAddress(instance, "glDrawArrays"));
 
         FreeLibrary(instance);
