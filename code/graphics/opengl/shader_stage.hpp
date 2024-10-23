bc672b90e3f4f3e085f78f79cc90e672bc221b94
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-23 23:04:09 +0300

    Add ShaderStage class to manage shader stages in OpenGL
    Introduced the `ShaderStage` class which provides functionalities to create, destroy, and manage shader sources. Updated `CMakeLists.txt` to include the new `shader_stage.cpp` file.

---
+++ code/graphics/opengl/shader_stage.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "object.hpp"
+
+namespace engine::gl
+{
+    class ShaderStage final : public Object
+    {
+    public:
+    explicit ShaderStage(uint32_t type);
+
+        void create()  override;
+        void destroy() override;
+
+        void source(const std::vector<char>& source) const;
+
+    private:
+        uint32_t type_ { };
+    };
+}
