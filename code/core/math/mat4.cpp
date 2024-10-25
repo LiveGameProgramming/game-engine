5376e5459390540900071b8b9150fcbca4df6d1e
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-25 22:33:25 +0300

    Add vec3 utilities and integrate with existing math library
    Included the vec3 class with constructors in the math library. Updated mat4 class to use vec3 for scale and translation functions. Adjusted CMakeLists to compile vec3 sources.

--- code/core/math/mat4.cpp
+++ code/core/math/mat4.cpp
@@ -10,6 +10,17 @@ namespace engine::math
         columns[3].w = 1.0f;
     }
 
+    void mat4::scale(const vec3& scale)
+    {
+        columns[0] *= scale.x;
+        columns[1] *= scale.y;
+        columns[2] *= scale.z;
+    }
+
+    void mat4::translate(const vec3& translation)
+    {
+    }
+
     mat4::operator const float*() const
     {
         return &columns[0].x;
