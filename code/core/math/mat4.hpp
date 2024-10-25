a1440fcc3c205f779421ce330667f7ca4bcd704a
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-25 23:17:38 +0300

    Add perspective projection and matrix multiplication functions
    Implemented perspective projection and matrix multiplication in mat4 class. Added utility functions for calculating radians and tangent for internal use.

--- code/core/math/mat4.hpp
+++ code/core/math/mat4.hpp
@@ -12,7 +12,13 @@ namespace engine::math
         void scale(const     vec3& scale);
         void translate(const vec3& translation);
 
-        operator const float*() const;
+        void perspective(float fov, float aspect, float near, float far);
+
+        constexpr const column& operator[](int32_t index) const;
+        constexpr       column& operator[](int32_t index);
+
+        mat4 operator*(const mat4& other) const;
+             operator  const float*()     const;
 
     private:
         column columns[4] { };
