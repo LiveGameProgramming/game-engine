d1baa4d498cb4e61c090fffa0dc85c36e560a3e6
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-28 19:48:44 +0200

    Add new methods and operators to vec3 struct
    Introduce default constructor and mathematical methods including normalize, cross, dot, and length. Add operator overloads for subtraction and multiplication.

--- modules/math/core/vec3.hpp
+++ modules/math/core/vec3.hpp
@@ -4,8 +4,19 @@ namespace engine::core
 {
     struct vec3
     {
+        vec3() = default;
+
         vec3(float value);
         vec3(float x, float y, float z);
+
+        void normalize();
+
+        [[nodiscard]] vec3   cross(const vec3& other) const;
+        [[nodiscard]] float    dot(const vec3& other) const;
+        [[nodiscard]] float length()     const;
+
+              vec3 operator-(const  vec3& other) const;
+        const vec3 operator*=(const vec3& other) const;
 
         float x { };
         float y { };
