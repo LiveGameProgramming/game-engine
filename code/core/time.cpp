cc93ac7262c8a9e13e88117f0d98795138a7b115
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-28 21:15:22 +0200

    Add time management system to engine core
    Introduce a `Time` class in the engine core to handle initialization, updating, and duration calculations. These changes include new source files `time.cpp` and `time.hpp` and modifications to relevant CMake configuration files to integrate the new system.

---
+++ code/core/time.cpp
@@ -0,0 +1,33 @@
+#include "time.hpp"
+
+namespace engine::core
+{
+    void Time::init()
+    {
+        previous_time = start_time = time_clock::now();
+    }
+
+    void Time::update()
+    {
+        const auto current_time = time_clock::now();
+
+        total_time_   = calculate_duration(start_time,    current_time);
+        delta_time_   = calculate_duration(previous_time, current_time);
+        previous_time = current_time;
+    }
+
+    float Time::total_time()
+    {
+        return total_time_;
+    }
+
+    float Time::delta_time()
+    {
+        return delta_time_;
+    }
+
+    float Time::calculate_duration(const time_point& start, const time_point& end)
+    {
+        return std::chrono::duration<float>(end - start).count();
+    }
+}
