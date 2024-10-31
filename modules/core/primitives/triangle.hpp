caa456b6f23da9bc8068689728b34ff572897da5
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-31 19:10:09 +0200

    Add face template to geometry and new RGB and Triangle structs
    Updated the geometry template to include a face type, replacing indices with faces. Added a new RGB struct for color representation. Introduced a Triangle struct to define a primitive with three vertex indices.

---
+++ modules/core/primitives/triangle.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace engine::primitives
+{
+    struct triangle
+    {
+        static constexpr int32_t elements = 3;
+
+        uint32_t a;
+        uint32_t b;
+        uint32_t c;
+    };
+}
