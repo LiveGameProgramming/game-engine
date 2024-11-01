fe634ac401f67401d0baa91c0cb16c4a0a300eeb
iamdramaticleo <iamdramaticleo@gmail.com>
2024-11-01 12:08:25 +0200

    Add static method for white color in rgb struct
    This commit introduces a static method `white()` to the `rgb` struct, which returns an instance of `rgb` with all components set to 1.0f. This improves convenience when a predefined white color instance is needed.

--- modules/math/base/rgb.hpp
+++ modules/math/base/rgb.hpp
@@ -4,6 +4,12 @@ namespace engine::base
 {
     struct rgb
     {
+        #pragma region Constants
+
+        static constexpr rgb white() { return { 1.0f, 1.0f, 1.0f }; }
+
+        #pragma endregion
+
         float r;
         float g;
         float b;
