7e11ec49c7084db9761bffbe975077f195766aa8
iamdramaticleo <iamdramaticleo@gmail.com>
2024-11-04 22:49:24 +0200

    Add light structure and enhance RGB defaults
    Introduced a `light` struct to encapsulate light properties like `position`, `ambient`, and `color` in the engine data module. Provided default values for `rgb` struct members and added `black` color constant. Additionally, clarified the use of the `extra` field within the `vertex` struct in the editor module.

--- modules/math/rgb.hpp
+++ modules/math/rgb.hpp
@@ -7,11 +7,12 @@ namespace engine
         #pragma region Constants
 
         static constexpr rgb white() { return { 1.0f, 1.0f, 1.0f }; }
+        static constexpr rgb black() { return { 0.0f, 0.0f, 0.0f }; }
 
         #pragma endregion
 
-        float r;
-        float g;
-        float b;
+        float r { 1.0f };
+        float g { 1.0f };
+        float b { 1.0f };
     };
 }
