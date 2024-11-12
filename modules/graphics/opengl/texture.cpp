e1cc9fcad1878dee9802b03aaa60b2451a61916b
iamdramaticleo <iamdramaticleo@gmail.com>
2024-11-12 20:05:54 +0200

    Add format and level parameters to Texture class
    Introduced a `format_` member and reorganized the class constructor to include it. Replaced `data` method with `storage` method to conform to usage and added a level parameter to `update`. Added getter methods for format, width, and height for better encapsulation.

--- modules/graphics/opengl/texture.cpp
+++ modules/graphics/opengl/texture.cpp
@@ -4,8 +4,9 @@
 
 namespace engine::opengl
 {
-    Texture::Texture(const uint32_t target, const int32_t width, const int32_t height)
+    Texture::Texture(const uint32_t target, const uint32_t format, const int32_t width, const int32_t height)
         : target  { target }
+        , format_ { format }
         , width_  { width  }
         , height_ { height }
     {
@@ -21,18 +22,33 @@ namespace engine::opengl
         glDeleteTextures(1, &handle_);
     }
 
-    void Texture::data(const uint32_t format) const
+    void Texture::storage(const int32_t levels) const
     {
-        glTextureStorage2D(handle_, 1, format, width_, height_);
+        glTextureStorage2D(handle_, levels, format_, width_, height_);
     }
 
-    void Texture::update(const uint32_t format, const void* pixels) const
+    void Texture::update(const uint32_t format, const void* pixels, const int32_t level) const
     {
-        glTextureSubImage2D(handle_, 0, 0, 0, width_, height_, format, type_ubyte, pixels);
+        glTextureSubImage2D(handle_, level, 0, 0, width_, height_, format, type_ubyte, pixels);
     }
 
     void Texture::bind(const uint32_t location) const
     {
         glBindTextureUnit(location, handle_);
+    }
+
+    uint32_t Texture::format() const
+    {
+        return format_;
+    }
+
+    int32_t Texture::width() const
+    {
+        return width_;
+    }
+
+    int32_t Texture::height() const
+    {
+        return height_;
     }
 }
--- modules/graphics/opengl/texture.hpp
+++ modules/graphics/opengl/texture.hpp
@@ -7,19 +7,25 @@ namespace engine::opengl
     class Texture final : public Object
     {
     public:
-        Texture(uint32_t target, int32_t width, int32_t height);
+        Texture(uint32_t target, uint32_t format, int32_t width, int32_t height);
 
         void create()  override;
         void destroy() override;
 
-        void   data(uint32_t format) const; // TODO name this storage?
-        void update(uint32_t format, const void* pixels) const;
+        void storage(int32_t levels = 1) const;
+        void update(uint32_t format,     const void* pixels, int32_t level = 0) const;
 
-        void   bind(uint32_t location = 0) const;
+        void bind(uint32_t location = 0) const;
+
+        [[nodiscard]] uint32_t format()  const;
+        [[nodiscard]]  int32_t  width()  const;
+        [[nodiscard]]  int32_t height()  const;
 
     private:
-        uint32_t target { };
-        int32_t  width_ { };
-        int32_t height_ { };
+        uint32_t target  { };
+        uint32_t format_ { };
+
+        int32_t  width_  { };
+        int32_t height_  { };
     };
 }
