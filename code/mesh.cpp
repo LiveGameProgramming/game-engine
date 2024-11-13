2f177d85f6884072983520e5aaf43118b4e13b7a
iamdramaticleo <iamdramaticleo@gmail.com>
2024-11-13 21:32:04 +0200

    Add Mesh class and integrate vertex array improvements
    Introduce the Mesh class to manage geometry data, and update methods in VertexArray for buffer handling using pointers and assertions. This ensures better resource management and consistency within the engine module.

---
+++ code/mesh.cpp
@@ -0,0 +1,49 @@
+#include "mesh.hpp"
+
+namespace engine
+{
+    Mesh::Mesh(const uint32_t primitive)
+        : primitive { primitive }
+    {
+    }
+
+    void Mesh::create(const int32_t stride)
+    {
+        vertex_buffer = std::make_unique<opengl::Buffer>();
+        index_buffer  = std::make_unique<opengl::Buffer>();
+
+        vertex_buffer->create();
+         index_buffer->create();
+
+        vertex_array = std::make_unique<opengl::VertexArray>();
+        vertex_array->create();
+
+        vertex_array->attach_vertices(vertex_buffer.get(), stride);
+        vertex_array->attach_indices(index_buffer.get());
+    }
+
+    void Mesh::destroy() const
+    {
+         index_buffer->destroy();
+        vertex_buffer->destroy();
+         vertex_array->destroy();
+    }
+
+    void Mesh::attributes(const std::vector<vertex::attribute>& attributes) const
+    {
+        for (const auto& attribute : attributes)
+        {
+            vertex_array->attribute(attribute);
+        }
+    }
+
+    void Mesh::bind() const
+    {
+        vertex_array->bind();
+    }
+
+    int32_t Mesh::faces() const
+    {
+        return faces_;
+    }
+}
