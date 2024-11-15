26d34278a9d4b33fa2caf448fc0b30b442d168fe
iamdramaticleo <iamdramaticleo@gmail.com>
2024-11-15 18:45:23 +0200

    Add `rect` struct and organize headers in math module
    Introduced `rect` struct in `rect.hpp` for defining rectangular areas using `vec2` points. Additionally, organized includes in `headers.hpp` under a pragma region for better readability and maintenance.

--- modules/math/headers.hpp
+++ modules/math/headers.hpp
@@ -4,3 +4,17 @@
 
 #include <cstring>
 #include <cmath>
+
+#pragma region Headers
+
+#include "rgb.hpp"
+
+#include "vec3.hpp"
+#include "vec2.hpp"
+
+#include "mat4.hpp"
+#include "quat.hpp"
+
+#include "rect.hpp"
+
+#pragma endregion
