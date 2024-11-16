8041fffb33685ed718fac7a5535a3355f58a4d7e
iamdramaticleo <iamdramaticleo@gmail.com>
2024-11-16 22:47:20 +0200

    Refactor namespaces and filenames for math and buffer classes
    Renamed buffer_data to buffer::data and updated relevant includes and calls. Updated the usage of math namespace for vector operations across multiple files for consistency.

--- code/graphics/model_renderer.cpp
+++ code/graphics/model_renderer.cpp
@@ -14,15 +14,15 @@ namespace engine::graphics
         _shader->push(matrix);
 
         texture->bind();
-                                                                                          mesh->bind();
+                                                                                                mesh->bind();
         opengl::Commands::draw_indexed(opengl::triangles, core::primitive::triangle::elements * mesh->faces());
     }
 
     void ModelRenderer::draw(const core::Mesh* mesh, const math::mat4& matrix, const math::rgb& color) const
     {
         _shader->push(matrix);
-        _buffer->update(core::buffer_data::create(&color));
-                                                                                          mesh->bind();
+        _buffer->update(core::buffer::data::create(&color));
+                                                                                                mesh->bind();
         opengl::Commands::draw_indexed(opengl::triangles, core::primitive::triangle::elements * mesh->faces());
     }
 
